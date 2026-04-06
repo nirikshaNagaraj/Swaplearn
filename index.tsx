846d6caf0bac9206f2a3af78d992cf10e879f42e
Niriksha Acharya <nirikshanagaraj09@gmail.com>
2026-04-06 22:26:22 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,5 +1,87 @@
+import React, { useState } from 'react';
+import Home from '../screens/Home';
+import About from '../screens/About';
+import Discover from '../screens/Discover';
+import Match from '../screens/Match';
+import Login from '../screens/Login';
 import Register from '../screens/Register';
 
 export default function Index() {
-  return <Register />;
+  const [screen, setScreen] = useState('home');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+    setScreen('home');
+  };
+if (screen === 'login') {
+  return (
+    <Login
+      switchToRegister={() => setScreen('register')}
+      onLoginSuccess={handleLoginSuccess}
+      goBack={() => setScreen('discover')} // 🔥 ADD THIS
+    />
+  );
+}
+
+  if (screen === 'register') {
+    return (
+      <Register switchToLogin={() => setScreen('login')} />
+    );
+  }
+
+ if (screen === 'about') {
+  return (
+    <About
+      isLoggedIn={isLoggedIn}
+      goToHome={() => setScreen('home')}
+      goToAbout={() => setScreen('about')}
+      goToDiscover={() => setScreen('discover')}
+      goToMatch={() => setScreen('match')}
+      goToLogin={() => setScreen('login')}
+      goToRegister={() => setScreen('register')}
+    />
+  );
+}
+
+if (screen === 'discover') {
+  return (
+    <Discover
+      isLoggedIn={isLoggedIn}
+      goToHome={() => setScreen('home')}
+      goToAbout={() => setScreen('about')}
+      goToDiscover={() => setScreen('discover')}
+      goToMatch={() => setScreen('match')}
+      goToLogin={() => setScreen('login')}
+      goToRegister={() => setScreen('register')}
+    />
+  );
+}
+
+if (screen === 'match') {
+  return (
+    <Match
+      isLoggedIn={isLoggedIn}
+      goToHome={() => setScreen('home')}
+      goToAbout={() => setScreen('about')}
+      goToDiscover={() => setScreen('discover')}
+      goToMatch={() => setScreen('match')}
+      goToLogin={() => setScreen('login')}
+      goToRegister={() => setScreen('register')}
+    />
+  );
+}
+  
+
+return (
+  <Home
+    isLoggedIn={isLoggedIn}
+    goToLogin={() => setScreen('login')}
+    goToRegister={() => setScreen('register')}
+    goToHome={() => setScreen('home')}
+    goToAbout={() => setScreen('about')}
+    goToDiscover={() => setScreen('discover')}
+    goToMatch={() => setScreen('match')}
+  />
+);
 }
