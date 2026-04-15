e297087684968018a5ca0bd7ece963b6a21ccd42
Niriksha Acharya <nirikshanagaraj09@gmail.com>
2026-04-15 19:53:57 +0530

    Add files via upload

--- Backend/server/urls.py
+++ Backend/server/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('api/', include('users.urls')),  # ✅ THIS LINE IS REQUIRED
+]
